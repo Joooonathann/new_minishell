3326c8a0d453b2097bd929212bf54d1923e94a01
Jonathan <peliium.2022@gmail.com>
2024-09-21 16:26:05 +0200

    fix pwd

--- src/builtins/commands/pwd_command.c
+++ src/builtins/commands/pwd_command.c
@@ -6,7 +6,7 @@
 /*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 22:21:39 by jalbiser          #+#    #+#             */
-/*   Updated: 2024/09/20 16:03:03 by jalbiser         ###   ########.fr       */
+/*   Updated: 2024/09/21 16:25:42 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 void	pwd_command(t_minishell **data)
 {
-	printf("%s\n", get_vars(&(*data)->env, "PWD")->value);
+	char	cwd[PATH_MAX];
+
+	getcwd(cwd, sizeof(cwd));
+	if (get_vars(&(*data)->env, "PWD"))
+		printf("%s\n", get_vars(&(*data)->env, "PWD")->value);
+	else
+		printf("%s\n", cwd);
 	exit(0);
 }
